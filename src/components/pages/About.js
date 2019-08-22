02224108f7c3c78405f4658679daa25dd4948f29
eswar <eswarsaladi041@gmail.com>
2019-08-22 22:19:44 +0530

    first commit

---
+++ src/components/pages/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const About = params => {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>This is todo list app from brad traversy media tutorial</p>
+    </div>
+  );
+};
+
+export default About;
